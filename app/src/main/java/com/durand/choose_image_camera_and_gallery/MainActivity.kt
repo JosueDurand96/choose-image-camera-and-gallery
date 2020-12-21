3151da124844c32641fe99c58d00e8203b8f5e2a
Consultor Dev 6 <consultordev6@quimicasuiza.com>
2020-12-21 14:16:43 -0500

    photo gallery

--- app/src/main/java/com/durand/choose_image_camera_and_gallery/MainActivity.kt
+++ app/src/main/java/com/durand/choose_image_camera_and_gallery/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.durand.choose_image_camera_and_gallery
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.app.Activity
+import android.content.ContentUris
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private var filePhoto: File?=null
+    companion object {
+        private val IMAGE_CHOOSE = 1000;
+        private val PERMISSION_CODE = 1001;
+        private const val REQUEST_CODE = 13
+        private const val FILE_NAME = "photo.jpg"
+
+
+    }
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btnCapture.setOnClickListener {
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+         //   filePhoto = getPhotoFile(Companion.FILE_NAME)
+         //   val providerFile =FileProvider.getUriForFile(this,"com.durand.choose_image_camera_and_gallery", filePhoto!!)
+         //   takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+        }
+
+
+        btnChoose.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else{
+                    val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                    startActivityForResult(gallery, pickImage)
+                }
+            }else{
+                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, pickImage)
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            mImageView.setImageURI(imageUri)
+        }
+    }
+
+
 }
