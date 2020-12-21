e2f8f1583a2a1efad9a75df92d887ed41027e0fa
Consultor Dev 6 <consultordev6@quimicasuiza.com>
2020-12-21 14:44:13 -0500

    capture

--- app/src/main/java/com/durand/choose_image_camera_and_gallery/MainActivity.kt
+++ app/src/main/java/com/durand/choose_image_camera_and_gallery/MainActivity.kt
@@ -4,8 +4,10 @@ import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
 import android.content.ContentUris
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
@@ -17,27 +19,42 @@ import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.common.ResizeOptions
+import com.facebook.imagepipeline.request.ImageRequestBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private var filePhoto: File?=null
+    private var filePhoto: File? = null
+
     companion object {
         private val IMAGE_CHOOSE = 1000;
         private val PERMISSION_CODE = 1001;
-        private const val REQUEST_CODE = 13
         private const val FILE_NAME = "photo.jpg"
-
-
     }
+
+    private val TAKE_PHOTO_REQUEST = 101
+    private var mCurrentPhotoPath: String = ""
     lateinit var imageView: ImageView
     lateinit var button: Button
     private val pickImage = 100
     private var imageUri: Uri? = null
+
+    val REQUEST_CODE = 200
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,36 +63,91 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCapture.setOnClickListener {
-            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-         //   filePhoto = getPhotoFile(Companion.FILE_NAME)
-         //   val providerFile =FileProvider.getUriForFile(this,"com.durand.choose_image_camera_and_gallery", filePhoto!!)
-         //   takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+            capturePhoto()
         }
 
 
         btnChoose.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
                     val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
                     requestPermissions(permissions, PERMISSION_CODE)
-                } else{
-                    val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                } else {
+                    val gallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
                     startActivityForResult(gallery, pickImage)
                 }
-            }else{
-                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            } else {
+                val gallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
                 startActivityForResult(gallery, pickImage)
             }
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == pickImage) {
-            imageUri = data?.data
-            mImageView.setImageURI(imageUri)
+//        if (resultCode == RESULT_OK && requestCode == pickImage) {
+//            imageUri = data?.data
+//            mImageView.setImageURI(imageUri)
+//        } else
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            mImageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
         }
     }
+
+    fun capturePhoto() {
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+
+
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/*")
+        val fileUri = contentResolver
+            .insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values
+            )
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            mCurrentPhotoPath = fileUri.toString()
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            )
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        }
+    }
+
+//    private fun processCapturedPhoto() {
+//        val cursor = contentResolver.query(
+//            Uri.parse(mCurrentPhotoPath),
+//            Array(1) { android.provider.MediaStore.Images.ImageColumns.DATA },
+//            null, null, null
+//        )
+//        cursor!!.moveToFirst()
+//        val photoPath = cursor.getString(0)
+//        cursor.close()
+//        val file = File(photoPath)
+//        val uri = Uri.fromFile(file)
+//
+//        val height = resources.getDimensionPixelSize(R.dimen.photo_height)
+//        val width = resources.getDimensionPixelSize(R.dimen.photo_width)
+//
+//        val request = ImageRequestBuilder.newBuilderWithSource(uri)
+//            .setResizeOptions(ResizeOptions(width, height))
+//            .build()
+//        val controller = Fresco.newDraweeControllerBuilder()
+//            .setOldController(mImageView?.controller)
+//            .setImageRequest(request)
+//            .build()
+//        mImageView?.controller = controller
+//    }
 
 
 }
