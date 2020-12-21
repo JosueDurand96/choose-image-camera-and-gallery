983c00e164319024e7af346690d6697dbe0b6c7c
Consultor Dev 6 <consultordev6@quimicasuiza.com>
2020-12-21 12:04:20 -0500

    first

---
+++ app/src/main/java/com/durand/choose_image_camera_and_gallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.durand.choose_image_camera_and_gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
